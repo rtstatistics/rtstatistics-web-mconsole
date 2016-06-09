b8924a73c19f50607fd2475aa842e661f666867d
James Hu <james.hu.ustc@hotmail.com>
2016-06-09 12:14:00 +1000

    move material design icons font to local

--- src/vendor.ts
+++ src/vendor.ts
@@ -9,13 +9,18 @@ import '@angular/router';
 import 'rxjs';
 
 // Material
-import '@angular2-material/button'
-import '@angular2-material/card'
-import '@angular2-material/checkbox'
-import '@angular2-material/core'
-import '@angular2-material/icon'
-import '@angular2-material/input'
-import '@angular2-material/sidenav'
-import '@angular2-material/tabs'
+import '@angular2-material/button';
+import '@angular2-material/card';
+import '@angular2-material/checkbox';
+import '@angular2-material/core';
+import '@angular2-material/icon';
+import '@angular2-material/input';
+import '@angular2-material/sidenav';
+import '@angular2-material/tabs';
+
+// Material Design Icons
+import 'material-design-icons';
+require('../node_modules/material-design-icons/iconfont/material-icons.css')
+
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
