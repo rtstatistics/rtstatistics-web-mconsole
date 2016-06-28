90576541e510e69f98533a232205d374ee674b1d
James Hu <james.hu.ustc@hotmail.com>
2016-06-28 14:54:04 +1000

    add data model for statistics periods hierarchy

--- src/models/periods-hierarchy.ts
+++ src/models/periods-hierarchy.ts
@@ -1,4 +1,17 @@
 import {Asset} from './asset';
 
 export class PeriodsHierarchy extends Asset{
+    periods: Period[];
 } 
+
+export class Period{
+    amount: number;
+    configuration: PeriodConfiguration;
+    timeZone: string;
+    unit: string;
+    upperLevelPeriods: Period[];
+}
+
+export class PeriodConfiguration{
+    keepWithinMinutes: number;
+}
